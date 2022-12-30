e240b6697383d5d47062126e0d3caf81209abb5c
Rumkokos05 <borsodi.milan05@gmail.com>
2022-12-30 19:36:23 +0100

    Csibesz Django

---
+++ Ablog/models.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    Gun = models.CharField(max_length=16)
+    Caliber = models.CharField(max_length=20)
+    Country = models.CharField(max_length=56)
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.Gun
